289b0a4ebba2424a104a7d5bc227e803af0f97ed
Heark <hearkofsorcerey@gmail.com>
2015-05-19 18:15:34 -0400

    Update groudon.js

--- groudon.js
+++ groudon.js
@@ -399,7 +399,7 @@ beforeChannelMessage: function(message, channel, html) {
         var tier = vData[1];
       //  var rank = client.network().getRanking(tier, name);
      if (client.playerExist(clientid) == true){
-         client.network().sendChanMessage(channel,  client.seeRanking(tier, clientid)+".");
+         client.network().sendChanMessage(channel,  client.seeRanking(clientid)+".");
      } else {
          client.network().sendChanMessage(channel, "Error! Player: "+name+" does not exist!");
      }
