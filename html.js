9624051aa9121ce0ef1077e8e2cbbc9dbe393897
Heark <hearkofsorcerey@gmail.com>
2015-07-16 01:37:54 -0400

    Create html.js

---
+++ html.js
@@ -0,0 +1,10 @@
+sys.setAnnouncement("<b><table width='100%' border-color: #1871d4;'>
+<body style="background-color: qlineargradient(y1:0,x1:0,x2:1,y2:0 stop:0
+orange,stop:.1 white,stop:.5 orange, stop:.9 white,stop:1
+orange)"><tr><td><br>
+<br><img src="pokemon:num=91&shiny=false"></td><td><img src="pokemon:num=282-1&shiny=false" align=left><img src='pokemon:num=491&shiny=true' align=right>
+<center><font face='Ketchum' font size='12'>Based Gods</font></center>
+<hr><center>Links: <a href="http://fullmetal.boards.net/thread/51/based-gods-league-originally-oora">League Info</a> | </font><a href="https://pokemonshowdown.com/damagecalc/"> Damage Calculator</a> | <a href="http://fullmetal.boards.net/">Forums</a> | <a href="http://fullmetal.boards.net/board/9/rate-team">Rate my teams</a></center><hr><center><font size='7'></font></center>
+<center>"+sys.time()+"<br> 
+<td>
+<br><img src='' align=right></td></tr></table> </b>")
