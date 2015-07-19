272cd7eb881592cfc1ba906a7a886a1a5625af79
Heark <hearkofsorcerey@gmail.com>
2015-07-19 15:28:38 -0400

    Update build.js

--- build.js
+++ build.js
@@ -413,7 +413,37 @@ poScript = ({
                         client.network().sendChanMessage(channel, vgBotName + "You don't have permission to use this command");
                     }
                 }
+             if (vCommand == "translate") {
+                    	var vData2 = vCommandData.split(":", 2);
+                        var tWord = vData2[0];
+                        var tLang = vData2[1];
+                        if(tLang == undefined){
+                        	client.network().sendChanMessage(channel, "Please define a language first. Use ~langs to see the supported languages")
+                        } else if(tLang.toLowerCase() == "spanish"){
+                        	tLang = "spa"
+                        }
+                        
+                           var loadtWord = sys.synchronousWebCall("https://glosbe.com/gapi/translate?from=eng&dest="+(encodeURIComponent(tLang))+"&format=json&phrase="+(encodeURIComponent(tWord))+"&pretty=false")
+                           var translated = JSON.parse(loadtWord)
+                           print(loadtWord)
+                           var tString = []
+                           var vDefLength = -1;
+                            var vCheck = true;
+                            for (x = 0; vCheck == true; x++) {
+                                try {
+                         tString[x] = translated.tuc[x].text;
+                                } catch (err) {
+                                    vCheck = false;
+                                }
+                            }
+                            var tStringToPrint = tString[0];
+                           if(tStringToPrint == undefined ){
+                           	client.network().sendChanMessage(channel, "Failed to load data for "+tWord)
+                           } else {
+                           	client.network().sendChanMessage(channel, tStringToPrint);
+                           }
 
+                    }
                 if (vCommand == "unban") {
 
                     if (controllers.indexOf(vUserSentName.toLowerCase()) !== -1) {
